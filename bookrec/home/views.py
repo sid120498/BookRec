4152e7e98bcf00a0b28112ca43f31e48174c3dd4
Siddharth Singh <sinsid91200@gmail.com>
2018-02-19 23:13:13 +0530

    Project Initialised

--- bookrec/home/views.py
+++ bookrec/home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def home(request):
+    if request.method =='GET':
+        return render(request,'home/home.html')
